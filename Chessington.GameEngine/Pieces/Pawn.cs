c4c6709ef5e38f57594bbcf3f5341375e6db1b91
RoryPyman <rory.pyman@gmx.com>
2025-06-25 15:01:33 +0100

    Implemented moving pawns forward 2

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -1,23 +1,43 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        public Pawn(Player player) 
+        public Pawn(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            Square postition = board.FindPiece(this);
+            Square position = board.FindPiece(this);
             int offset = this.Player == Player.White ? -1 : 1;
-            if (postition.Row > 8)
+            if (position.Row > 8)
             {
                 return Enumerable.Empty<Square>();
             }
-            Square new_loc = new Square(postition.Row + offset, postition.Col);
-            return board.GetPiece(new_loc) == null ? new Square[] { new_loc } : Enumerable.Empty<Square>();
+
+            List<Square> allMoves = new List<Square>();
+            Square new_loc = new Square(position.Row + offset, position.Col);
+            Square new_unmoved_loc = new Square(position.Row + offset * 2, position.Col);
+
+            if (board.GetPiece(new_loc) == null) allMoves.Add(new_loc);
+            if (IsStartingPosition(position, this.Player) && board.GetPiece(new_unmoved_loc) == null) allMoves.Add(new_unmoved_loc);
+            return allMoves.ToArray();
+        }
+
+        private bool IsStartingPosition(Square position, Player player)
+        {
+            if (player == Player.White && position.Row == 7)
+            {
+                return true;
+            }
+            if (player == Player.Black && position.Row == 1)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
