7c0ed3d079f368fd58af9e087834eef62fa4fb7f
RoryPyman <rory.pyman@gmx.com>
2025-06-25 14:40:24 +0100

    Made pawns move!

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,14 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square postition = board.FindPiece(this);
+            int offset = this.Player == Player.White ? -1 : 1;
+            if (postition.Row > 8)
+            {
+                return Enumerable.Empty<Square>();
+            }
+            Square new_loc = new Square(postition.Row + offset, postition.Col);
+            return board.GetPiece(new_loc) == null ? new Square[] { new_loc } : Enumerable.Empty<Square>();
         }
     }
 }
